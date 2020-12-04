eb087d797153ed60c95e164b291bf5022676a31f
chows23 <tsrn34388644@gmail.com>
2020-12-05 05:21:52 +0800

    update the gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 const {src, dest, series, parallel} = require('gulp');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+
+const babel = require('gulp-babel');
+
+const gulpUglify = require('gulp-uglify');
+
 
 function htmlTask() {
   return src('src/*.html')
@@ -7,17 +18,34 @@ function htmlTask() {
 
 function styleTask() {
   return src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer(), cssnano() ]))
+    .pipe(sourcemaps.write())
+    .pipe(concat('all.css'))
     .pipe(dest('dist/css'))
 }
 
 function scriptTask() {
   return src('src/js/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulpUglify())
+    .pipe(concat('all.js'))
+
     .pipe(dest('dist/js'))
 }
 
 function imageTask() {
   return src('src/images/*png')
+    .pipe(imagemin())
     .pipe(dest('dist/img'))
 }
+
+
+exports.images = imageTask;
+exports.html = htmlTask;
+exports.scripts = scriptTask;
+exports.styles = styleTask;
 
 exports.default = series(htmlTask, imageTask, styleTask, scriptTask);
